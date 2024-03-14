4eee7f9b77160f3dba25913f996c56b4fc2b13c7
Emily N <emnnichols@outlook.com>
2024-03-14 11:11:01 -0700

    Created view for project list

---
+++ src/components/projects-view/projects-view.jsx
@@ -0,0 +1,44 @@
+import { React } from "react";
+import PropTypes from "prop-types";
+import { Button, Card, Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export const ProjectsView = ({ project }) => {
+
+  return (
+    <>
+      <Card className="h-100 w-100">
+        <Card.Body className="cardBody">
+          <Card.Title className="cardTitle" style={{ letterSpacing: "1px" }}>{project.name}</Card.Title>
+          <Card.Text className="projectBlurb">{project.blurb}</Card.Text>
+        </Card.Body>
+
+        <Card.Img variant="top" src={project.image} className="h-100 w-100 projectImage" />
+
+        <Card.Body className="cardBody">
+          <Card.Text className="justify-content-md-center">
+            <Row className="justify-content-md-center">
+              <Col md={6} sm={12} className="w-100">
+                <Link to={`/projects/${encodeURIComponent(project.name)}`}>
+                  <Button variant="primary" className="primaryButton mt-2">DETAILS</Button>
+                </Link>
+                <Link to={project.github} target="_blank">
+                  <Button variant="primary" className="primaryButton mt-2">GITHUB</Button>
+                </Link>
+              </Col>
+            </Row>
+          </Card.Text>
+        </Card.Body>
+      </Card >
+    </>
+  );
+};
+
+ProjectsView.propTypes = {
+  project: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    desc: PropTypes.string.isRequired,
+    github: PropTypes.string.isRequired,
+  }).isRequired
+};
