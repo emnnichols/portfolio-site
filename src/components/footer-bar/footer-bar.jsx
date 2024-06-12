e6fd2cac4f25faf10c09b881cfc39d222581128d
Emily Nichols <emnnichols@outlook.com>
2024-06-12 15:28:31 -0700

    Updated footer

--- src/components/footer-bar/footer-bar.jsx
+++ src/components/footer-bar/footer-bar.jsx
@@ -1,42 +1,48 @@
-import { Row, Col, Container, Nav, Navbar } from "react-bootstrap";
+import { Row, Col, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-import github from "../../../img/github.svg";
-import linkedIn from "../../../img/linkedin.svg";
-import twitter from "../../../img/twitter.svg";
+import github from "../../../img/github.png";
+import linkedIn from "../../../img/linkedin.png";
+import twitter from "../../../img/twitter.png";
 
 export const Footer = () => {
 
   return (
     <>
-    <Row className="footer mt-5 w-100 justify-content-md-center">
-    <Col className="col-12" style={{marginLeft:"2%"}}>Find me at the following</Col>
+      <Row className="footer">
+        <Col className="col-12"><Navbar className="justify-content-center">
+          <Row>
+            <Nav>
+              <Col className="col-4">
+                <Nav.Link as={Link} to="https://github.com/emnnichols" target="_blank" >
+                  <img src={github} className="footerIcon" width="30px" aria-label="Github" alt="Github icon created by Freepik" />
+                </Nav.Link>
+              </Col>
 
-      <Navbar className="footer" style={{marginLeft:"3%"}}>
-        <Container>
-          <Row className="w-100 justify-content-md-center"><Col lg={2} className="col-12">
-          <Nav>
-                <Col className="col-4">
-                  <Nav.Link as={Link} to="https://github.com/emnnichols" target="_blank">
-                    <img src={github} alt="Github" />
-                  </Nav.Link>
-                </Col>
-                
-                <Col className="col-4">
-                  <Nav.Link as={Link} to="https://twitter.com/emnnichols" target="_blank">
-                    <img src={twitter} alt="Twxitter" />
-                  </Nav.Link>
-                </Col>
+              <Col className="col-4">
+                <Nav.Link as={Link} to="https://twitter.com/emnnichols" target="_blank">
+                  <img src={twitter} className="footerIcon" width="30px" aria-label="X formerly Twitter" alt="X icon created by Freepik" />
+                </Nav.Link>
+              </Col>
 
-                <Col className="col-4">
-                  <Nav.Link as={Link} to="https://www.linkedin.com/in/emily-nichols-724931296/" target="_blank">
-                    <img src={linkedIn} alt="LinkedIn" />
-                  </Nav.Link>
-                </Col>
-          </Nav></Col></Row>
-        </Container>
-      </Navbar>
-    </Row>
-    </>
+              <Col className="col-4">
+                <Nav.Link as={Link} to="https://www.linkedin.com/in/emily-nichols-724931296/" target="_blank">
+                  <img src={linkedIn} className="footerIcon" width="30px" aria-label="LinkedIn" alt="Linkedin icon created by Freepik" />
+                </Nav.Link>
+              </Col>
+            </Nav></Row>
+        </Navbar >
+        </Col>
+
+        <Col className="col-12"><Navbar className="iconAttribute justify-content-center">
+
+          <a href="https://www.flaticon.com/free-icons/linkedin" target="_blank" title="linkedin icons">Linkedin</a>
+          <span style={{ paddingRight: "5px" }}>,</span>
+          <a href="https://www.flaticon.com/free-icons/github" target="_blank" title="github icons">Github</a>
+          <span style={{ paddingRight: "5px" }}>,</span>
+          <a href="https://www.flaticon.com/free-icons/tweet" target="_blank" title="tweet icons" style={{ paddingRight: "5px" }}>X / Twitter</a> icons created by Freepik
+
+        </Navbar></Col>
+      </Row></>
   )
 };
